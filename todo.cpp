dd38c9b758c09aa76583cd374d9479e2fcfb1916
Lucas Azevedo <lucasbazev@gmail.com>
2024-05-18 00:50:17 -0400

    feat: undone command and validate command function | some small refactoring

--- todo.cpp
+++ todo.cpp
@@ -14,11 +14,26 @@ struct Task {
   bool done = false;
 };
 
+bool validateCommand(string command) {
+  string validCommands[] = {"add", "remove", "done", "undone", "list", "help", "end"};
+  bool valid = false;
+
+  for (string validCommand : validCommands) {
+    if (command == validCommand) {
+      valid = true;
+      return valid;
+    }
+  }
+
+  return valid;
+}
+
 int main() {
   string commands[] = {
     "'add <task_name>'   -> adds task to list",
     "'remove <task_id>'  -> removes task from list",
-    "'done <task_id>'    -> marks task as completed",
+    "'done <task_id>'    -> marks task as done",
+    "'undone <task_id>   -> marks task as undone",
     "'list'              -> prints current state of list",
     "'help'              -> lists available commands",
     "'end'               -> exits program",   
@@ -36,20 +51,14 @@ int main() {
     cout << ANSI_RESET << endl << "Enter a command: ";
     getline(cin, command);
 
-    if (command != "add" &&
-        command != "remove" &&
-        command != "done" &&
-        command != "list" &&
-        command != "help" &&
-        command != "end" &&
-        command != "") {
-      cout << "Invalid command. Enter 'help' to see a list of available commands." << endl;
+    if (!validateCommand(command)) {
+      cout << ANSI_RED << "Invalid command. Enter 'help' to see a list of available commands." << endl;
       continue;
     }
 
     if (currIndex == 0) {
-      if (command == "remove" || command == "done") {
-        cout << "Cannot remove or mark as done since your list is empty. Try adding a new task." << endl;
+      if (command == "remove" || command == "done" || command == "undone") {
+        cout << ANSI_YELLOW << "Cannot remove or mark as done/undone since your list is empty.\nTry adding a new task." << endl;
         continue;
       }
     }
@@ -61,8 +70,8 @@ int main() {
       task.id = currIndex + 1;
       todos[currIndex] = task;
       currIndex++;
-
-      printf("'%s' added to your list.\n", task.name.c_str());
+      
+      printf("%s'%s' added to your list.\n", ANSI_BLUE, task.name.c_str());
     }
 
     if (command == "done") {
@@ -76,8 +85,23 @@ int main() {
           todos[i].done = true;
 
           cout << ANSI_GREEN;
-          printf("Task '%i. %s' marked as completed.\n", todos[i].id, todos[i].name.c_str());
+          printf("Task '%i. %s' marked as done.\n", todos[i].id, todos[i].name.c_str());
           cout << "Well done!" << endl;
+        }
+      }
+    }
+
+    if (command == "undone") {
+      cout << "Enter task id: ";
+      cin >> taskId;
+      cin.ignore();
+      
+      for (int i = 0; i < currIndex; i++) {
+        if (todos[i].id == taskId) {
+          todos[i].done = true;
+
+          cout << ANSI_YELLOW;
+          printf("Task '%i, %s' marked as undone.\n", todos[i].id, todos[i].name.c_str());
         }
       }
     }
@@ -125,7 +149,7 @@ int main() {
     if (command == "help") {
       cout << endl << "Available commands:" << endl;
       
-      for (int i = 0; i < 6; i++) {
+      for (int i = 0; i < 7; i++) {
         cout << commands[i] << endl;
       }
 
