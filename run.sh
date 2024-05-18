ee3be7e0d568bc21cc328d4939af54369abe359a
Lucas Azevedo <lucasbazev@gmail.com>
2024-05-18 12:11:55 -0400

    feat: run.sh now tries to compile with both clang++ or g++

--- run.sh
+++ run.sh
@@ -1,2 +1,19 @@
-clang++ todo.cpp -o todo
-./todo
+#!/bin/bash
+
+# Try to compile with clang++
+clang++ todo.cpp -o todo_executable
+
+# If clang++ fails, try to compile with g++
+if [ $? -ne 0 ]; then
+	echo "clang++ failed, trying g++..."
+	g++ todo.cpp -o todo_executable
+
+	# Check if g++ succeeded
+	if [ $? -ne 0 ]; then
+		echo "Both clang++ and g++ failed to compile todo.cpp"
+		exit 1
+	fi
+fi
+
+# Run the executable if compilation succeeded
+./todo_executable
